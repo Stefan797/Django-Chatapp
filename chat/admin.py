c9028982f033d5dd6a7e558127d4962351e18e4e
Stefan797 <stefanhubner97@web.de>
2022-02-21 16:30:44 +0100

    added message in SQL Admin

--- chat/admin.py
+++ chat/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from .models import Message
 # Register your models here.
+
+class MessageAdmin(admin.ModelAdmin):    
+    fields = ('text','created_at', 'author', 'receiver')    
+    list_display = ('created_at', 'author', 'text', 'receiver')    
+    search_fields = ('text',)
+
+
+admin.site.register(Message, MessageAdmin)
